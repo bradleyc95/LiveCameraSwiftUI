0d8ce64690598d621637de8b61f1d71bd576a7a1
Bradley Cox <bradleycox1995@gmail.com>
2023-02-06 12:20:21 -0500

    Initial Commit

---
+++ LiveCameraSwiftUI/LiveCameraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCameraSwiftUIApp.swift
+//  LiveCameraSwiftUI
+//
+//  Created by Bradley Cox on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LiveCameraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
