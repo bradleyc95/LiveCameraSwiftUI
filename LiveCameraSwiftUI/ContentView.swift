b5e5ccf564134af1b21308497796369891af0672
bradleyc95 <bradleycox1995@gmail.com>
2023-02-14 12:52:20 -0500

    Changed input device to front camera

--- LiveCameraSwiftUI/ContentView.swift
+++ LiveCameraSwiftUI/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var model = FrameHandler()
+    let heightRatio: CGFloat = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            
+            ZStack{
+                Color.white
+                VStack{
+                    FrameView(image: model.frame)
+                        .frame(width: geo.size.width, height: geo.size.height*heightRatio, alignment: .leading)
+                       // .ignoresSafeArea()
+                    Spacer()
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
